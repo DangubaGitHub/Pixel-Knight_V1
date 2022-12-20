969d7710daeeb101f7c75f54b16684ff9552b7da
DangubaGitHub <dangubicjovan@outlook.com>
2022-12-20 13:17:25 +0100

    commit 052
    jumping zombie is finally finished

--- Pixel-Knight_V1/Assets/Scripts/Enemies/Jumping Zombie/JumpingZombieController.cs
+++ Pixel-Knight_V1/Assets/Scripts/Enemies/Jumping Zombie/JumpingZombieController.cs
@@ -15,6 +15,7 @@ public class JumpingZombieController : MonoBehaviour
     const string IDLE = "Jumping_Zombie_Idle";
     const string JUMP = "Jumping_Zombie_Jump";
     const string FALL = "Jumping_Zombie_Fall";
+    const string STILL = "Jumping_Zombie_Still";
 
     string currentState;
 
@@ -25,6 +26,14 @@ public class JumpingZombieController : MonoBehaviour
     [SerializeField] GameObject enemyDeathEffect;
 
     [SerializeField] GameObject Player;
+
+    ////////////////////  Activation  ///
+
+    [Header("Activation")]
+    [SerializeField] LayerMask PlayerLayer;
+    [SerializeField] Transform PlayerCheck;
+    [SerializeField] float PlayerCheckRadius;
+    [SerializeField] bool isActive;
 
     Rigidbody2D rb2d;
     Animator anim;
@@ -42,72 +51,61 @@ public class JumpingZombieController : MonoBehaviour
 
     void Update()
     {
-        ////////////////////  ANIMATIONS  ///
+        isActive = Physics2D.OverlapCircle(PlayerCheck.position, PlayerCheckRadius, PlayerLayer);
 
-        if (isGrounded)
+        if (isActive)
         {
-            ChangeAnimationState(IDLE);
-        }
+            ////////////////////  ANIMATIONS  ///
 
-        if(!isGrounded)
-        {
-            if(rb2d.velocity.y >= 0)
+            if (isGrounded)
             {
-                ChangeAnimationState(JUMP);
+                ChangeAnimationState(IDLE);
             }
 
-            else if(rb2d.velocity.y < 0)
+            if (!isGrounded)
             {
-                ChangeAnimationState(FALL);
+                if (rb2d.velocity.y >= 0)
+                {
+                    ChangeAnimationState(JUMP);
+                }
+
+                else if (rb2d.velocity.y < 0)
+                {
+                    ChangeAnimationState(FALL);
+                }
             }
-        }
 
-        ////////////////////  ENEMY LOOK DIRECTION AND FLIPPING  ///
+            ////////////////////  ENEMY LOOK DIRECTION AND FLIPPING  ///
 
-        Vector3 scale = transform.localScale;
+            Vector3 scale = transform.localScale;
 
-        if(Player.transform.position.x > transform.position.x)
-        {
-            scale.x = Mathf.Abs(scale.x);
+            if (Player.transform.position.x > transform.position.x)
+            {
+                scale.x = Mathf.Abs(scale.x);
+            }
+
+            else
+            {
+                scale.x = Mathf.Abs(scale.x) * -1;
+            }
+
+            transform.localScale = scale;
+
+            ////////////////////  NEXT JUMP DELAY  ///
+
+            if (Time.time > nextTime)
+            {
+                rb2d.velocity = new Vector2(rb2d.velocity.x, jumpForce);
+
+                nextTime = Time.time + nextJump;
+            }
         }
 
         else
         {
-            scale.x = Mathf.Abs(scale.x) * -1;
-        }
-
-        transform.localScale = scale;
-
-        ////////////////////  NEXT JUMP DELAY  ///
-
-        if (Time.time >nextTime)
-        {
-            rb2d.velocity = new Vector2(rb2d.velocity.x, jumpForce);
-
-            nextTime = Time.time + nextJump;
+            ChangeAnimationState(STILL);
         }
     }
-
-    /*private void OnTriggerEnter2D(Collider2D other)
-    {
-        if(other.CompareTag("Ground"))
-        {
-            isGrounded = true;
-        }
-
-        if(other.CompareTag("Player"))
-        {
-            rb2d.velocity = new Vector2(rb2d.velocity.x, jumpForce);
-        }
-    }
-
-    private void OnTriggerExit2D(Collider2D other)
-    {
-        if(other.CompareTag("Ground"))
-        {
-            isGrounded = false;
-        }
-    }*/
 
     private void OnCollisionEnter2D(Collision2D other)
     {
@@ -119,11 +117,6 @@ public class JumpingZombieController : MonoBehaviour
         if (other.gameObject.tag == "Player")
         {
             rb2d.velocity = new Vector2(rb2d.velocity.x, jumpForce);
-
-            /*if (PlayerAnimationManager.instance.isBasic == true && PlayerHealthController.instance.invincibleLength <= 0)
-            {
-                Instantiate(enemyDeathEffect, other.transform.position, other.transform.rotation);
-            }*/
         }
     }
 
