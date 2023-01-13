64c0b79a9fa2cf58b07da2a90e6aa797e7b7e09f
DangubaGitHub <dangubicjovan@outlook.com>
2023-01-13 23:19:12 +0100

    commit 091
    finished rotating skull enemy

---
+++ Pixel-Knight_V1/Assets/Scripts/Enemies/Skull/SkullController.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkullController : MonoBehaviour
+{
+    [SerializeField] Transform rotationCenter;
+    [SerializeField] float rotationSpeed;
+    [SerializeField] float rotationRadius;
+
+    float positionX;
+    float positionY;
+    float angle;
+
+    SpriteRenderer sr;
+
+    private void Awake()
+    {
+        sr = GetComponent<SpriteRenderer>();
+    }
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        positionX = rotationCenter.position.x + Mathf.Cos(angle) * rotationRadius;
+        positionY = rotationCenter.position.y + Mathf.Sin(angle) * rotationRadius;
+        transform.position = new Vector2(positionX, positionY);
+        angle = angle + Time.deltaTime * rotationSpeed;
+
+        if(angle >= 360)
+        {
+            angle = 0;
+        }
+
+        if (positionY < transform.parent.position.y)
+        {
+            sr.flipX = false;
+        }
+
+        else if (positionY > transform.parent.position.y)
+        {
+            sr.flipX = true;
+        }
+    }
+}
