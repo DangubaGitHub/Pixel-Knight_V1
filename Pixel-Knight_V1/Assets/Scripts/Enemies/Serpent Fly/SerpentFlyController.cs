6b7e9abeb82f352f512baba6a7d76c6cd525f6b1
DangubaGitHub <dangubicjovan@outlook.com>
2023-01-21 20:56:13 +0100

    commit 104
    finished serpent fly enemy and made prefab + extended test map

--- Pixel-Knight_V1/Assets/Scripts/Enemies/Serpent Fly/SerpentFlyController.cs
+++ Pixel-Knight_V1/Assets/Scripts/Enemies/Serpent Fly/SerpentFlyController.cs
@@ -4,13 +4,46 @@ using UnityEngine;
 
 public class SerpentFlyController : MonoBehaviour
 {
+    [SerializeField] Transform rotationCenter;
+    [SerializeField] float rotationSpeed;
+    [SerializeField] float rotationRadius;
+
+    float positionX;
+    float positionY;
+    float angle;
+
+    SpriteRenderer sr;
+
+    private void Awake()
+    {
+        sr = GetComponent<SpriteRenderer>();
+    }
+
     void Start()
     {
-        
+
     }
 
     void Update()
     {
-        
+        positionX = rotationCenter.position.x + Mathf.Cos(angle) * rotationRadius;
+        positionY = rotationCenter.position.y + Mathf.Sin(angle) * rotationRadius;
+        transform.position = new Vector2(positionX, positionY);
+        angle = angle + Time.deltaTime * rotationSpeed;
+
+        if (angle >= 360)
+        {
+            angle = 0;
+        }
+
+        if (positionY < transform.parent.position.y)
+        {
+            sr.flipX = false;
+        }
+
+        else if (positionY > transform.parent.position.y)
+        {
+            sr.flipX = true;
+        }
     }
 }
