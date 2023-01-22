71a3d1b86d67ac99a12b593c6fb0967891c88a62
DangubaGitHub <dangubicjovan@outlook.com>
2023-01-22 18:05:18 +0100

    commit 107
    reworked fire projectile behavior, still doesn't kill all enemies

--- Pixel-Knight_V1/Assets/Scripts/Player/Bullets/FireBall.cs
+++ Pixel-Knight_V1/Assets/Scripts/Player/Bullets/FireBall.cs
@@ -6,41 +6,98 @@ public class FireBall : MonoBehaviour
 {
     Rigidbody2D rb2d;
     Animator anim;
+    SpriteRenderer sr;
 
     string currentState;
     const string DESTROY = "fireball_destroy";
 
-    float force = 10;
+    [SerializeField] float VelocityX = 15;
+    [SerializeField] float VelocityY = 3;
+
+    bool isGrounded;
 
     [SerializeField] GameObject enemyDeathEffect;
+    [SerializeField] GameObject bulletDestroyAnimation;
 
     private void Awake()
     {
         rb2d = GetComponent<Rigidbody2D>();
         anim = GetComponent<Animator>();
+        sr = GetComponent<SpriteRenderer>();
     }
 
     void Start()
     {
-        rb2d.velocity = transform.right * force;
+        rb2d.velocity = transform.right * VelocityX;
         StartCoroutine(DelayDestroy());
         Invoke("DestroyBall", 3.2f);
     }
 
     void Update()
     {
-        
+        if (isGrounded)
+        {
+            rb2d.velocity = new Vector2(rb2d.velocity.x, VelocityY);
+        }
+
+    }
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.tag == "Enemy")
+        {
+            Destroy(gameObject);
+            Instantiate(enemyDeathEffect, other.transform.position, other.transform.rotation);
+            Destroy(other.gameObject);
+        }
+
+        if (other.gameObject.tag == "Ground" ||
+            other.gameObject.tag == "Ground 2" ||
+            other.gameObject.tag == "Spikes")
+        {
+            isGrounded = true;
+        }
+
+        if (other.gameObject.tag == "Wall")
+        {
+            ChangeDirection();
+        }
+
+        if (other.gameObject.tag == "Enemy Invulnerable Damaging" ||
+            other.gameObject.tag == "Enemy Invulnerable Bounce")
+        {
+            rb2d.velocity = new Vector2(0, 0);
+            Instantiate(bulletDestroyAnimation, transform.position, Quaternion.identity);
+            sr.enabled = false;
+            rb2d.constraints = RigidbodyConstraints2D.FreezeAll;
+            Invoke("DestroyBall", 1f);
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D other)
+    {
+        if (other.gameObject.tag == "Ground" ||
+            other.gameObject.tag == "Ground 2" ||
+            other.gameObject.tag == "Spikes")
+        {
+            isGrounded = false;
+        }
+    }
+
+    void ChangeDirection()
+    {
+        VelocityX = -VelocityX;
+    }
+
+    void DestroyBall()
+    {
+        Destroy(gameObject);
     }
 
     IEnumerator DelayDestroy()
     {
         yield return new WaitForSeconds(2);
         ChangeAnimationState(DESTROY);
-    }
-
-    void DestroyBall()
-    {
-        Destroy(gameObject);
     }
 
     void ChangeAnimationState(string newState)
@@ -52,13 +109,4 @@ public class FireBall : MonoBehaviour
         currentState = newState;
     }
 
-    void OnCollisionEnter2D(Collision2D other)
-    {
-        if (other.gameObject.tag == "Enemy")
-        {
-            Destroy(gameObject);
-            Instantiate(enemyDeathEffect, other.transform.position, other.transform.rotation);
-            Destroy(other.gameObject);
-        }
-    }
 }
