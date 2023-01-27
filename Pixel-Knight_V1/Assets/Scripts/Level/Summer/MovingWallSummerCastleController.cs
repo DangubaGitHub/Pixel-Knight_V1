9008ca3b7073c471bad74354213547929c15cf37
DangubaGitHub <dangubicjovan@outlook.com>
2023-01-27 14:31:05 +0100

    commit 113
    - finished summer castle moving wall
    - imported summer boss sprites

---
+++ Pixel-Knight_V1/Assets/Scripts/Level/Summer/MovingWallSummerCastleController.cs
@@ -0,0 +1,79 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingWallSummerCastleController : MonoBehaviour
+{
+    Rigidbody2D rb2d;
+    public static MovingWallSummerCastleController instance;
+
+    CapsuleCollider2D capsuleCollider;
+
+    //////////////////////////////////////////////////////////// Entrance //////////
+
+    [SerializeField] bool entrance;
+    [SerializeField] bool isInside;
+    [SerializeField] float entranceVelocityY;
+
+    //////////////////////////////////////////////////////////// Exit //////////
+
+    [SerializeField] bool exit;
+    [SerializeField] bool bossIsDead;
+    [SerializeField] float exitVelocityY;
+
+    private void Awake()
+    {
+        instance = this;
+        rb2d = GetComponent<Rigidbody2D>();
+        capsuleCollider = GetComponent<CapsuleCollider2D>();
+    }
+
+    void Update()
+    {
+        if (entrance)
+        {
+            capsuleCollider.enabled = capsuleCollider.enabled;
+
+            if (isInside)
+            {
+                EntranceMovingUp();
+
+                Invoke("StopMovement", 1.1f);
+            }
+        }
+
+        if (exit)
+        {
+            if (bossIsDead)
+            {
+                ExitMovingUp();
+
+                Invoke("StopMovement", 1.1f);
+            }
+        }
+    }
+
+    void EntranceMovingUp()
+    {
+        rb2d.velocity = new Vector2(0, -entranceVelocityY);
+    }
+
+    void ExitMovingUp()
+    {
+        rb2d.velocity = new Vector2(0, exitVelocityY);
+    }
+
+    void StopMovement()
+    {
+        rb2d.constraints = RigidbodyConstraints2D.FreezeAll;
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            isInside = true;
+            capsuleCollider.enabled = !capsuleCollider.enabled;
+        }
+    }
+}
